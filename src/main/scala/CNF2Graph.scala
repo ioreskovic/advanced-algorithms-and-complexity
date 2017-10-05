e6fcbd72205298f216c382ad7184c3f1584f0d8b
eivaore <ivan.oreskovic@ericsson.com>
2017-10-05 13:00:20 +0200

    Graph methods
    Postorder traversal
    Strongly connected components

--- src/main/scala/CNF2Graph.scala
+++ src/main/scala/CNF2Graph.scala
@@ -1,4 +1,12 @@
+import CNF2Graph.{SCC, Vertex}
+
+import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
+
 object CNF2Graph {
+  type Vertex = Int
+  type SCC = Int
+
   trait Term {
     def variable: Int
     def sign: Boolean
@@ -57,11 +65,71 @@ object CNF2Graph {
   case class CNF2EdgePair(e0: CNF2Edge, e1: CNF2Edge)
 
   def apply(nVariables: Int)(lines: Seq[String]): CNF2Graph = {
-    val adjMap = SAT2(lines.map(s => CNF(s))).edgePairs.flatMap(ep => Seq(ep.e0, ep.e1)).foldLeft(Map[Int, List[Int]]().withDefaultValue(List())) { case (adj, e) => adj + (e.fromIndex -> (e.toIndex :: adj(e.fromIndex))) }
+    val adjMap = SAT2(lines.map(s => CNF(s))).edgePairs.flatMap(ep => Seq(ep.e0, ep.e1)).foldLeft(Map[Vertex, List[Vertex]]().withDefaultValue(Nil)) { case (adj, e) => adj + (e.fromIndex -> (e.toIndex :: adj(e.fromIndex))) }
     CNF2Graph(nVariables * 2, adjMap)
   }
 }
 
-case class CNF2Graph(nVertices: Int, adj: Map[Int, List[Int]]) {
-  
+case class CNF2Graph(nVertices: Int, adj: Map[Vertex, List[Vertex]]) {
+  def transpose: CNF2Graph = {
+    val newAdj = adj.foldLeft(Map[Vertex, List[Vertex]]().withDefaultValue(Nil)){
+      case (adjT, (from, tos)) => tos.foldLeft(adjT){
+        case (adjTi, to) => adjTi + (to -> (from :: adjTi(to)))
+      }
+    }
+
+    CNF2Graph(nVertices, newAdj)
+  }
+
+  def neighbours(vertex: Vertex): List[Vertex] = adj(vertex)
+  def apply(vertex: Vertex): List[Vertex] = neighbours(vertex)
+  def hasEdge(from: Vertex, to: Vertex): Boolean = adj(from).contains(to)
+
+  def scc: Map[SCC, List[Vertex]] = {
+    import scala.collection.mutable.{HashMap => MutableHashMap, HashSet => MutableHashSet}
+    val reversePostOrder = postOrder.reverse
+    val sccMap = MutableHashMap[SCC, List[Vertex]]().withDefaultValue(Nil)
+    val gT = transpose
+    val visited = MutableHashSet[Vertex]()
+
+    def explore(vertex: Vertex, scc: SCC): Unit = {
+      visited(vertex) = true
+      sccMap.update(scc, vertex :: sccMap(scc))
+      gT(vertex).foreach(n => if (!visited(n)) explore(n, scc))
+    }
+
+    @tailrec
+    def loop(vx: List[Vertex], scc: SCC = 0): Unit = vx match {
+      case v :: vs if !visited(v) => {
+        explore(v, scc)
+        loop(vs, scc + 1)
+      }
+      case _ :: vs => loop(vs, scc)
+      case Nil =>
+    }
+
+    loop(reversePostOrder)
+    sccMap.toMap
+  }
+
+  def postOrder: List[Vertex] = {
+    import scala.collection.mutable.{HashSet => MutableHashSet}
+    val order = ListBuffer[Vertex]()
+    val visited = MutableHashSet[Vertex]()
+
+    def dfsLoop(vertex: Vertex): Unit = {
+      if (!visited(vertex)) {
+        visited(vertex) = true
+        adj(vertex).foreach(neighbour => dfsLoop(neighbour))
+        order.append(vertex)
+      }
+    }
+
+    for (vertex <- 0 until nVertices) {
+      dfsLoop(vertex)
+    }
+
+    order.toList
+  }
+
 }
