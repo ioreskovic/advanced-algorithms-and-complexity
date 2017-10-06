30aadf95567bdfdd3e8a0e515081a0874660c23d
eivaore <ivan.oreskovic@ericsson.com>
2017-10-06 15:04:48 +0200

    Added default empty adj lists and companion apply

--- src/main/scala/week4/wip/DirectedGraph.scala
+++ src/main/scala/week4/wip/DirectedGraph.scala
@@ -14,12 +14,27 @@ class DirectedGraph[Vertex](val vertices: List[Vertex], val adj: Map[Vertex, Lis
       }
     }
 
-    new DirectedGraph(vertices, transposedAdj.toMap)
+    new DirectedGraph(vertices, transposedAdj.toMap.withDefaultValue(Nil))
+  }
+
+  override lazy val toString: String = {
+    adj.mkString(s"Graph{$vertices", "\n", "\n")
   }
 }
 
 object DirectedGraph {
-  def apply[Vertex](edges: List[Edge[Vertex]])(implicit conversion: Vertex => Int): DirectedGraph[Vertex] = {
+  def apply[Vertex](vertices: Seq[Vertex], edges: Seq[Edge[Vertex]])(implicit conversion: Vertex => Int): DirectedGraph[Vertex] = {
+    import scala.collection.mutable.{ HashMap => MutableHashMap }
+    val adjMap = MutableHashMap[Vertex, List[Vertex]]().withDefaultValue(Nil)
+
+    for (e <- edges) {
+      adjMap(e.from) = e.to :: adjMap(e.from)
+    }
+
+    new DirectedGraph(vertices.toList, adjMap.toMap.withDefaultValue(Nil))
+  }
+
+  def apply[Vertex](edges: Seq[Edge[Vertex]])(implicit conversion: Vertex => Int): DirectedGraph[Vertex] = {
     import scala.collection.mutable.{ HashSet => MutableHashSet, HashMap => MutableHashMap }
     val vertexSet = MutableHashSet[Vertex]()
     val adjMap = MutableHashMap[Vertex, List[Vertex]]().withDefaultValue(Nil)
@@ -30,6 +45,6 @@ object DirectedGraph {
       adjMap(e.from) = e.to :: adjMap(e.from)
     }
 
-    new DirectedGraph(vertexSet.toList, adjMap.toMap)
+    new DirectedGraph(vertexSet.toList, adjMap.toMap.withDefaultValue(Nil))
   }
 }
