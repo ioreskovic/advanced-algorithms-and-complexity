057f06bf11f7cf30db16dbaeaa9eab6e63008597
eivaore <ivan.oreskovic@ericsson.com>
2017-10-06 10:07:48 +0200

    Constructing directed graph from edges

--- src/main/scala/week4/wip/DirectedGraph.scala
+++ src/main/scala/week4/wip/DirectedGraph.scala
@@ -1,5 +1,7 @@
 package week4.wip
 
+import week4.wip.Graph.Edge
+
 case class DirectedGraph[Vertex](vertices: List[Vertex], adj: Map[Vertex, List[Vertex]])(implicit conversion: Vertex => Int) extends Graph[Vertex] {
   override def transpose: DirectedGraph[Vertex] = {
     import scala.collection.mutable.{ HashMap => MutableHashMap }
@@ -17,4 +19,17 @@ case class DirectedGraph[Vertex](vertices: List[Vertex], adj: Map[Vertex, List[V
 }
 
 object DirectedGraph {
+  def apply[Vertex](edges: List[Edge[Vertex]])(implicit conversion: Vertex => Int): DirectedGraph[Vertex] = {
+    import scala.collection.mutable.{ HashSet => MutableHashSet, HashMap => MutableHashMap }
+    val vertexSet = MutableHashSet[Vertex]()
+    val adjMap = MutableHashMap[Vertex, List[Vertex]]().withDefaultValue(Nil)
+
+    for (e <- edges) {
+      vertexSet(e.from) = true
+      vertexSet(e.to) = true
+      adjMap(e.from) = e.to :: adjMap(e.from)
+    }
+
+    DirectedGraph(vertexSet.toList, adjMap.toMap)
+  }
 }
